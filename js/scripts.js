540689f0303f9704ff9a5b612b31c0b273e65235
Alina <asalisinasa@gmail.com>
2016-02-07 01:04:45 +0300

    edit grids, add portfolio section and feedback form

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-// http://www.funnyjellyfish.ru/
+// $(window).scroll(function() {
+
+//   var st = $(this).scrollTop();
+
+//   $(".intro").css( {
+//     "transform" : "translate(0%, -" + st/5 + "%"
+//   });
+
+// });
+
+// $(window).scroll(function() {
+
+//   var st = $(this).scrollTop();
+
+//   $(".features__wrap").css( {
+//     "transform" : "translate(0%, -" + st/5 + "%"
+//   });
+
+// });
+
+
+
+
+// // http://www.funnyjellyfish.ru/
 
 
 
@@ -16,98 +39,98 @@
     });
 
 
-    /*-------------------------------------------------------------------------------
-      Parallax
-    -------------------------------------------------------------------------------*/
+//     /*-------------------------------------------------------------------------------
+//       Parallax
+//     -------------------------------------------------------------------------------*/
 
 
 
-    $(window).stellar({
-      responsive: true,
-      horizontalScrolling: false,
-      hideDistantElements: false,
-      horizontalOffset: 0,
-      verticalOffset: 0,
-    });
+//     $(window).stellar({
+//       responsive: true,
+//       horizontalScrolling: false,
+//       hideDistantElements: false,
+//       horizontalOffset: 0,
+//       verticalOffset: 0,
+//     });
 
 
 
-    /*-------------------------------------------------------------------------------
-     Navbar collapse
-    -------------------------------------------------------------------------------*/
+//     /*-------------------------------------------------------------------------------
+//      Navbar collapse
+//     -------------------------------------------------------------------------------*/
 
 
 
-    var navbar=$('.navbar');
-    var navbarAffixHeight=56
+//     var navbar=$('.navbar');
+//     var navbarAffixHeight=56
 
 
-    $('.navbar-collapse').on('show.bs.collapse', function () {
-        navbar.addClass('affix');
-    });
+//     $('.navbar-collapse').on('show.bs.collapse', function () {
+//         navbar.addClass('affix');
+//     });
 
-    $('.navbar-collapse').on('hidden.bs.collapse', function () {
-        if (navbar.hasClass('affix-top')){
-            navbar.removeClass('affix');
-        }
+//     $('.navbar-collapse').on('hidden.bs.collapse', function () {
+//         if (navbar.hasClass('affix-top')){
+//             navbar.removeClass('affix');
+//         }
             
-    });
+//     });
 
-    $(".navbar-nav > li > a").on('click', function() {
-        $(".navbar-collapse").collapse('hide');
-    });
-
-
-
-    /*-------------------------------------------------------------------------------
-     Affix
-    -------------------------------------------------------------------------------*/
+//     $(".navbar-nav > li > a").on('click', function() {
+//         $(".navbar-collapse").collapse('hide');
+//     });
 
 
 
-    navbar.affix({
-      offset: {
-        top:1
-      }
-    });
+//     /*-------------------------------------------------------------------------------
+//      Affix
+//     -------------------------------------------------------------------------------*/
 
-    navbar.on('affixed-top.bs.affix', function() {
-        if ($('.navbar-collapse').hasClass('in')){
-            navbar.addClass('affix');
-        }   
-    });
+
+
+//     navbar.affix({
+//       offset: {
+//         top:1
+//       }
+//     });
+
+//     navbar.on('affixed-top.bs.affix', function() {
+//         if ($('.navbar-collapse').hasClass('in')){
+//             navbar.addClass('affix');
+//         }   
+//     });
 
     
 
 
-    /*-------------------------------------------------------------------------------
-      Smooth scroll to anchor
-    -------------------------------------------------------------------------------*/
+//     /*-------------------------------------------------------------------------------
+//       Smooth scroll to anchor
+//     -------------------------------------------------------------------------------*/
 
 
 
 
-    $('.js-scroller').on('click', function(e) {
-        e.preventDefault();
-        var target = $(this.hash);
-        if (target.length) {
-            $('html,body').animate({
-                scrollTop: (target.offset().top - navbarAffixHeight + 1)
-            }, 1000);
-        }
-    });
+//     $('.js-scroller').on('click', function(e) {
+//         e.preventDefault();
+//         var target = $(this.hash);
+//         if (target.length) {
+//             $('html,body').animate({
+//                 scrollTop: (target.offset().top - navbarAffixHeight + 1)
+//             }, 1000);
+//         }
+//     });
 
 
 
 
-    /*-------------------------------------------------------------------------------
-     Scrollspy
-    -------------------------------------------------------------------------------*/
+//     /*-------------------------------------------------------------------------------
+//      Scrollspy
+//     -------------------------------------------------------------------------------*/
 
 
 
-    $('body').scrollspy({
-        offset:  navbarAffixHeight + 1
-    });
+//     $('body').scrollspy({
+//         offset:  navbarAffixHeight + 1
+//     });
     
 
